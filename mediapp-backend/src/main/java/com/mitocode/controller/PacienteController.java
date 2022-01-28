3a141473edf4027db34dff5b9a8a0202cee1ac4c
landrea-velez <landrea.velez@gmail.com>
2022-01-28 09:05:45 -0500

    [Add] Refactor controller status servicios
    Se controlan los mensajes de estados de los servicios

--- mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
+++ mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
@@ -1,8 +1,11 @@
 package com.mitocode.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mitocode.Service.IPacienteService;
 import com.mitocode.model.Paciente;
@@ -24,28 +28,35 @@ public class PacienteController {
 
 	@GetMapping
 	// @RequestMapping(value = "/", method = RequestMethod.GET)
-	public List<Paciente> listar() throws Exception {
-		return service.listar();
+	public ResponseEntity<List<Paciente>> listar() throws Exception {
+		List<Paciente> lista = service.listar();
+		return new ResponseEntity<>(lista, HttpStatus.OK);
 	}
 
 	@GetMapping("/{id}")
-	public Paciente listarPorId(@PathVariable("id") Integer id) throws Exception {
-		return service.listarPorId(id);
+	public ResponseEntity<Paciente> listarPorId(@PathVariable("id") Integer id) throws Exception {
+		Paciente paciente = service.listarPorId(id);
+		return new ResponseEntity<>(paciente, HttpStatus.OK);
 	}
 
 	@PostMapping
-	public Paciente registrar(@RequestBody Paciente p) throws Exception {
-		return service.registrar(p);
+	public ResponseEntity<Void> registrar(@RequestBody Paciente p) throws Exception {
+		Paciente paciente = service.registrar(p);
+		//localhost:8080/pacientes/5
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(paciente.getIdPaciente()).toUri();
+		return  ResponseEntity.created(location).build();
 	}
 
 	@PutMapping
-	public Paciente modificar(@RequestBody Paciente p) throws Exception {
-		return service.modificar(p);
+	public ResponseEntity<Paciente> modificar(@RequestBody Paciente p) throws Exception {
+		Paciente paciente = service.modificar(p);
+		return new ResponseEntity<>(paciente, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")
-	public void eliminar(@PathVariable("id") Integer id) throws Exception {
+	public ResponseEntity<Void> eliminar(@PathVariable("id") Integer id) throws Exception {
 		service.eliminar(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 }

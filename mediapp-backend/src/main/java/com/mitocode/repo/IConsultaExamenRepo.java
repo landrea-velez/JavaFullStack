d09a17dd50fdb124b4e4b71f3df70e75cce091f6
landrea-velez <landrea.velez@gmail.com>
2022-01-31 21:54:19 -0500

    [Add] guardado transaccional

--- mediapp-backend/src/main/java/com/mitocode/repo/IConsultaExamenRepo.java
+++ mediapp-backend/src/main/java/com/mitocode/repo/IConsultaExamenRepo.java
@@ -3,7 +3,6 @@ package com.mitocode.repo;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.mitocode.model.ConsultaExamen;
 
